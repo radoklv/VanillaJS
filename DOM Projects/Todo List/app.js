68178d4e2cb7b1aa7139b9064078c383ba31a417
radoklv <developmentemail07@gmail.com>
2021-02-25 15:50:22 +0200

    todo list JS optimized

--- DOM Projects/Todo List/app.js
+++ DOM Projects/Todo List/app.js
@@ -1,104 +1,181 @@
 // ****** SELECT ITEMS **********
-const alert = document.querySelector(".alert");
 const form = document.querySelector(".grocery-form");
-const grocery = document.getElementById("grocery");
 const submitBtn = document.querySelector(".submit-btn");
-const container = document.querySelector(".grocery-container");
-const list = document.querySelector(".grocery-list");
+const itemsList = document.querySelector(".grocery-list");
+const listContainer = document.querySelector(".grocery-container");
 const clearBtn = document.querySelector(".clear-btn");
+const alert = document.querySelector(".alert");
 
 // edit option
-let editElement;
+let editElement = undefined;
 let editFlag = false;
-let editID = "";
+let editId = null;
 
 // ****** EVENT LISTENERS **********
-//dubmit form
-form.addEventListener("submit", addItem);
+window.addEventListener("DOMContentLoaded", initApp);
 
-clearBtn.addEventListener("click", clearItems);
-
-// ****** FUNCTIONS **********
-function addItem(e) {
+form.addEventListener("submit", (e) => {
   e.preventDefault();
-
-  const value = grocery.value;
-  const id = new Date().getTime().toString();
+  const value = form.grocery.value.trim();
 
   if (value && !editFlag) {
-    const element = document.createElement("article");
+    let id = new Date().getTime().toString();
 
-    element.classList.add("grocery-item");
-
-    const attr = document.createAttribute("data-id");
-
-    attr.value = id;
-
-    element.setAttributeNode(attr);
-
-    element.innerHTML = `
-        <p class="title">${value}</p>
-        <div class="btn-container">
-          <button type="button" class="edit-btn">
-            <i class="fas fa-edit"></i>
-          </button>
-
-          <button type="button" class="delete-btn">
-            <i class="fas fa-trash"></i>
-          </button>`;
-    list.appendChild(element);
-
-    displayAlert("item added to the list", "success");
-
-    //show container
-    container.classList.add("show-container");
-
-    //add to local storage
+    addElement(id, value);
     addToLocalStorage(id, value);
 
-    //set back to default
-    setBackToDefault();
+    if (!listContainer.classList.contains("show-container")) {
+      listContainer.classList.add("show-container");
+    }
   } else if (value && editFlag) {
+    const updatedVal = form.grocery.value.trim();
+    updateElement(editElement, updatedVal);
   } else {
-    displayAlert("please enter value", "danger");
+    alertMessage("There is empty Value!", false);
+  }
+
+  reset();
+});
+
+itemsList.addEventListener("click", (e) => {
+  const parent = e.target.parentElement.parentElement.parentElement;
+
+  if (e.target.classList.contains("fa-edit")) {
+    submitBtn.textContent = "update";
+    editFlag = true;
+    const title = parent.querySelector(".title");
+    editId = parent.getAttribute("data-id");
+    form.grocery.value = title.textContent;
+    editElement = parent;
+  }
+
+  if (e.target.classList.contains("fa-trash")) {
+    deleteFromLocalStorage(parent.getAttribute("data-id"));
+    parent.remove();
+
+    if (itemsList.children.length == 0) {
+      listContainer.classList.remove("show-container");
+    }
+
+    reset();
+  }
+});
+
+clearBtn.addEventListener("click", deleteList);
+
+
+
+// ****** FUNCTIONS **********
+
+function initApp() {
+  let todos = getLocalStorage();
+  if (todos) {
+    todos.forEach((todo) => {
+      addElement(todo.id, todo.title);
+    });
+  }
+
+  if (itemsList.children.length) {
+    listContainer.classList.add("show-container");
   }
 }
 
-//display alert
-function displayAlert(text, action) {
-  alert.textContent = text;
-  alert.classList.add(`alert-${action}`);
+function updateElement(parent, newVal) {
+  parent.querySelector(".title").textContent = newVal;
+  const todos = getLocalStorage();
+  const updatedTodos = todos.map((todo) => {
+    if (todo.id == editId) {
+      return { id: todo.id, title: newVal };
+    } else {
+      return todo;
+    }
+  });
+  setLocalStorage(updatedTodos);
+  alertMessage("Todo was updated successful!");
+  reset();
+}
 
-  //remove alert
+function addElement(id, value) {
+  const html = `
+  <article class="grocery-item" data-id='${id}'>
+    <p class="title">${value}</p>
+
+    <div class="btn-container">
+      <button type="button" class="edit-btn">
+        <i class="fas fa-edit"></i>
+      </button>
+
+      <button type="button" class="delete-btn">
+        <i class="fas fa-trash"></i>
+      </button>
+    </div>
+  </article>`;
+
+  alertMessage("Todo was added successful!");
+  itemsList.innerHTML += html;
+}
+
+function deleteList() {
+  itemsList.innerHTML = "";
+  localStorage.removeItem("todos");
+  if (itemsList.children.length == 0) {
+    listContainer.classList.remove("show-container");
+  }
+  alertMessage("Todo List was cleared successful!");
+  reset();
+}
+
+function addToLocalStorage(id, value) {
+  if (!localStorage.getItem("todos")) {
+    const newObj = [{ id: id, title: value }];
+    setLocalStorage(newObj);
+  } else {
+    let todos = getLocalStorage();
+    todos.push({
+      id: id,
+      title: value,
+    });
+    setLocalStorage(todos);
+  }
+}
+
+function deleteFromLocalStorage(id) {
+  let todos = getLocalStorage();
+  todos = todos.filter((todo) => {
+    return todo.id != id;
+  });
+  localStorage.setItem("todos", JSON.stringify(todos));
+
+  alertMessage("Todo was deleted successful!");
+}
+
+function alertMessage(message, bgColor = true) {
+  if (bgColor) {
+    alert.classList.add("alert-success");
+  } else {
+    alert.classList.add("alert-danger");
+  }
+
+  alert.textContent = `${message}`;
   setTimeout(() => {
+    alert.classList.remove("alert-success");
+    alert.classList.remove("alert-danger");
     alert.textContent = "";
-    alert.classList.remove(`alert-${action}`);
   }, 1000);
 }
 
-
-//clear items
-function clearItems(){
-    const items = document.querySelectorAll('.grocery-item');
-
-    if(items.length > 0){
-        items.forEach( item =>{
-            list.removeChild(item)
-        })
-    }
-
-    container.classList.remove("show-container");
-    displayAlert("empty list", "danger")
+function getLocalStorage() {
+  return JSON.parse(localStorage.getItem("todos"));
 }
 
-//set back to default
-function setBackToDefault() {
-  grocery.value = "";
+function setLocalStorage(obj) {
+  localStorage.setItem("todos", JSON.stringify(obj));
+}
+
+function reset() {
+  editElement = undefined;
   editFlag = false;
-  editID = "";
-  submitBtn.textContent = "submit";
+  editId = null;
+  form.reset();
+  submitBtn.textContent = "save";
 }
-// ****** LOCAL STORAGE **********
-function addToLocalStorage(id, value) {}
-
-// ****** SETUP ITEMS **********
